f0c8f9779f1ee21e46f4071249dd6cb5e3caf502
Victor Sand <victor.sand@gmail.com>
2011-12-04 12:18:06 -0800

    testcases

---
+++ code/test/experiment.cpp
@@ -0,0 +1,142 @@
+#include <string>
+#include <ostream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <map>
+#include "Debug.h"
+#include "Instance.h"
+#include "Exception.h"
+#include <vector>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+
+int main(int argc, char *argv[]) {
+    try {
+
+        /*
+        Construct a shipping network with at least four locations, 
+        and confirm that shipments move between them as expected. 
+        Run tests with different segment capacity values to compare 
+        your network under light and heavy congestion. Add a diagram 
+        of this network to your README file, and discuss the tests performed. 
+        The code for this client should be named verification.cpp.
+
+        In addition, set up a shipping network with 100 sources and 1 destination. 
+        The destination should be connected to a terminal, which is in turn connected 
+        to 10 terminals, each of which is connected to 10 sources. All connections are 
+        via truck segments. Have each source send shipments holding 100 packages to the 
+        destination at the normal truck speed. Run the simulation for a while.
+
+        Now, reset the simulation and do the same thing, but vary the shipment sizes 
+        (use a randomly chosen shipment size for each source) from 1 package to 1,000 
+        packages. Record the results for each case.
+
+        Discuss in your README file the level of shipment delivery 
+        refusal observed in each case. The client implementing this test 
+        should be submitted as experiment.cpp. We would like you to include 
+        whatever statistics you think are relevant to your specific implementation, 
+        but at a bare minimum please provide us with the destination's shipments 
+        received and average latency, as well as some sort of measure of segment
+        performance (an average for each of shipments received and refused, for example).
+
+        */
+
+        Ptr<Instance::Manager> manager = shippingInstanceManager();
+        Ptr<Instance> stats = manager->instanceNew("myStats", "Stats");
+        Ptr<Instance> conn = manager->instanceNew("myConn", "Conn");
+        Ptr<Instance> fleet = manager->instanceNew("myFleet", "Fleet");
+
+        fleet->attributeIs("Boat, speed", "20");
+        fleet->attributeIs("Boat, capacity", "1000");
+        fleet->attributeIs("Boat, cost", "30");
+        fleet->attributeIs("Truck, speed", "60");
+        fleet->attributeIs("Truck, capacity", "50");
+        fleet->attributeIs("Truck, cost", "20");
+        fleet->attributeIs("Plane, speed", "700");
+        fleet->attributeIs("Plane, capacity", "200");
+        fleet->attributeIs("Plane, cost", "60");
+
+        std::map<string, Ptr<Instance> > terminals;
+        std::map<string, Ptr<Instance> > segments;
+        std::map<string, Ptr<Instance> > sources;
+
+        Ptr<Instance> destination = manager->instanceNew("destination", "Customer");
+        terminals["firstTerminal"] = manager->instanceNew("firstTerminal", "Truck terminal");
+
+        segments["destTermSegA"] = manager->instanceNew("destTermSegA", "Truck segment");
+        segments["destTermSegB"] = manager->instanceNew("destTermSegB", "Truck segment");
+        segments["destTermSegA"]->attributeIs("source", "destination");
+        segments["destTermSegB"]->attributeIs("source", "firstTerminal");
+        segments["destTermSegA"]->attributeIs("return segment", "destTermSegB");
+
+        // create the first layer of terminals
+        std::ostringstream s;
+        for (int i=0; i<10; ++i) {
+            s.str("");
+            string terminalName = "truckTerminal";
+            s << i;
+            terminalName.append(s.str());
+            terminals[terminalName] = manager->instanceNew(terminalName, "Truck terminal");
+            string segmentNameA = "firstLayerSegA";
+            segmentNameA.append(s.str());
+            string segmentNameB = "firstLayerSegB";
+            segmentNameB.append(s.str());
+            segments[segmentNameA] = manager->instanceNew(segmentNameA, "Truck segment");
+            segments[segmentNameB] = manager->instanceNew(segmentNameB, "Truck segment");
+            segments[segmentNameA]->attributeIs("source", "firstTerminal");
+            segments[segmentNameB]->attributeIs("source", terminalName);
+            segments[segmentNameA]->attributeIs("return segment", segmentNameB);
+
+            // 10 sources for each terminal
+            for (int j=0; j<10; ++j) {
+
+                s.str("");
+                int rate = 20;
+                s << rate;
+                string transferRate = s.str();
+
+                s.str("");
+                int size = 100;
+                s << size;
+                string shipmentSize = s.str();
+
+                s.str("");
+                s << i;
+                string index = s.str();
+                s.str("");
+                s << j;
+                index.append(s.str());
+
+                string sourceName = "source";
+                sourceName.append(index);
+                sources[sourceName] = manager->instanceNew(sourceName, "Customer");
+                sources[sourceName]->attributeIs("transfer rate", transferRate);
+                sources[sourceName]->attributeIs("shipment size", shipmentSize);
+                sources[sourceName]->attributeIs("destination", "destination");
+
+                string segmentNameA = "secondLayerSegA";
+                segmentNameA.append(index);
+                string segmentNameB = "secondLayerSegB";
+                segmentNameB.append(index);
+                segments[segmentNameA] = manager->instanceNew(segmentNameA, "Truck segment");
+                segments[segmentNameB] = manager->instanceNew(segmentNameB, "Truck segment");
+                segments[segmentNameA]->attributeIs("source", terminalName);
+                segments[segmentNameB]->attributeIs("source", sourceName);
+                segments[segmentNameA]->attributeIs("return segment", segmentNameB);
+            }
+        }
+
+        // print some stats
+        cout << "STATS:" << endl;
+        cout << "Destination shipments recieved: " << destination->attribute("shipments recieved") << endl;
+        cout << "Destination average latency: " << destination->attribute("average latency") << endl;
+
+    } catch (Fwk::Exception e) {
+        std::cerr << "Exception in main: " << e.what() << std::endl;
+    }
+    return 0;
+}
---
+++ code/test/verification.cpp
@@ -0,0 +1,176 @@
+#include <string>
+#include <ostream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <map>
+#include "Debug.h"
+#include "Instance.h"
+#include "Exception.h"
+#include <vector>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+
+int main(int argc, char *argv[]) {
+    try {
+
+        /*
+        Construct a shipping network with at least four locations, 
+        and confirm that shipments move between them as expected. 
+        Run tests with different segment capacity values to compare 
+        your network under light and heavy congestion. Add a diagram 
+        of this network to your README file, and discuss the tests performed. 
+        The code for this client should be named verification.cpp.
+        */
+
+        Ptr<Instance::Manager> manager = shippingInstanceManager();
+        Ptr<Instance> stats = manager->instanceNew("myStats", "Stats");
+        Ptr<Instance> conn = manager->instanceNew("myConn", "Conn");
+        Ptr<Instance> fleet = manager->instanceNew("myFleet", "Fleet");
+        Ptr<Instance> timeManager = manager->instanceNew("myTimeManager", "Time manager");
+
+        fleet->attributeIs("Boat, speed", "20");
+        fleet->attributeIs("Boat, speedAlt", "20");
+        fleet->attributeIs("Boat, capacity", "1000");
+        fleet->attributeIs("Boat, capacity", "1000");
+        fleet->attributeIs("Boat, cost", "30");
+        fleet->attributeIs("Boat, costAlt", "50");
+        fleet->attributeIs("Truck, speed", "60");
+        fleet->attributeIs("Truck, speedAlt", "90");
+        fleet->attributeIs("Truck, capacity", "50");
+        fleet->attributeIs("Truck, capacityAlt", "30");
+        fleet->attributeIs("Truck, costAlt", "50");
+        fleet->attributeIs("Truck, cost", "20");
+        fleet->attributeIs("Plane, speed", "700");
+        fleet->attributeIs("Plane, capacity", "200");
+        fleet->attributeIs("Plane, cost", "60");
+        fleet->attributeIs("Plane, speedAlt", "600");
+        fleet->attributeIs("Plane, capacityAlt", "200");
+        fleet->attributeIs("Plane, costAlt", "100");
+
+        fleet->attributeIs("altTimeStart", "10");
+        fleet->attributeIs("altTimeEnd", "22");
+
+        Ptr<Instance> timmernabben = manager->instanceNew("Timmernabben", "Customer");
+        Ptr<Instance> oskarshamn = manager->instanceNew("Oskarshamn", "Port");
+        Ptr<Instance> gotland = manager->instanceNew("Gotland", "Customer");
+        Ptr<Instance> goteborg = manager->instanceNew("Goteborg", "Port");
+        Ptr<Instance> hyssna = manager->instanceNew("Hyssna", "Customer");
+        Ptr<Instance> jonkoping = manager->instanceNew("Jonkoping", "Truck terminal");
+        Ptr<Instance> vaxjo = manager->instanceNew("Vaxjo", "Truck terminal");
+        Ptr<Instance> dh = manager->instanceNew("DH", "Customer");
+
+        timmernabben->attributeIs("transfer rate", "10");
+        timmernabben->attributeIs("shipment size", "100");
+        timmernabben->attributeIs("destination", "DH");
+
+        hyssna->attributeIs("transfer rate", "20");
+        hyssna->attributeIs("shipment size", "50");
+        hyssna->attributeIs("destination", "Gotland");
+
+        Ptr<Instance> tim_osk = manager->instanceNew("tim_osk", "Truck segment");
+        Ptr<Instance> osk_tim = manager->instanceNew("osk_tim", "Truck segment");
+        tim_osk->attributeIs("return segment", "osk_tim");
+        tim_osk->attributeIs("source", "Timmernabben");
+        osk_tim->attributeIs("source", "Oskarshamn");
+        tim_osk->attributeIs("length", "50");
+        osk_tim->attributeIs("length", "50");
+        tim_osk->attributeIs("capacity", "2");
+        osk_tim->attributeIs("capacity", "2");
+
+        Ptr<Instance> osk_gotl = manager->instanceNew("osk_gotl", "Boat segment");
+        Ptr<Instance> gotl_osk = manager->instanceNew("gotl_osk", "Boat segment");
+        osk_gotl->attributeIs("return segment", "gotl_osk");
+        osk_gotl->attributeIs("source", "Oskarshamn");
+        gotl_osk->attributeIs("source", "Gotland");
+        osk_gotl->attributeIs("length", "130");
+        gotl_osk->attributeIs("length", "130");
+        osk_gotl->attributeIs("capacity", "2");
+        gotl_osk->attributeIs("capacity", "2");
+
+        Ptr<Instance> osk_jon = manager->instanceNew("osk_jon", "Truck segment");
+        Ptr<Instance> jon_osk = manager->instanceNew("jon_osk", "Truck segment");
+        osk_jon->attributeIs("return segment", "jon_osk");
+        osk_jon->attributeIs("source", "Oskarshamn");
+        jon_osk->attributeIs("source", "Jonkoping");
+        osk_jon->attributeIs("length", "180");
+        jon_osk->attributeIs("length", "180");
+        osk_jon->attributeIs("capacity", "5");
+        jon_osk->attributeIs("capacity", "5");
+        
+        Ptr<Instance> tim_vax = manager->instanceNew("tim_vax", "Truck segment");
+        Ptr<Instance> vax_tim = manager->instanceNew("vax_tim", "Truck segment");
+        tim_vax->attributeIs("return segment", "vax_tim");
+        tim_vax->attributeIs("source", "Timmernabben");
+        vax_tim->attributeIs("source", "Vaxjo");
+        tim_vax->attributeIs("length", "120");
+        vax_tim->attributeIs("length", "120");
+        tim_vax->attributeIs("capacity", "3");
+        vax_tim->attributeIs("capacity", "3");
+
+        Ptr<Instance> vax_jon = manager->instanceNew("vax_jon", "Truck segment");
+        Ptr<Instance> jon_vax = manager->instanceNew("jon_vax", "Truck segment");
+        jon_vax->attributeIs("return segment", "vax_jon");
+        jon_vax->attributeIs("source", "Jonkoping");
+        vax_jon->attributeIs("source", "Vaxjo");
+        jon_vax->attributeIs("length", "120");
+        vax_jon->attributeIs("length", "120");
+        jon_vax->attributeIs("capacity", "3");
+        vax_jon->attributeIs("capacity", "3");
+
+        Ptr<Instance> jon_dh = manager->instanceNew("jon_dh", "Truck segment");
+        Ptr<Instance> dh_jon = manager->instanceNew("dh_jon", "Truck segment");
+        jon_dh->attributeIs("return segment", "dh_jon");
+        jon_dh->attributeIs("source", "Jonkoping");
+        dh_jon->attributeIs("source", "DH");
+        jon_dh->attributeIs("length", "5");
+        dh_jon->attributeIs("length", "5");
+        jon_dh->attributeIs("capacity", "2");
+        dh_jon->attributeIs("capacity", "2");
+
+        Ptr<Instance> jon_gote = manager->instanceNew("jon_gote", "Truck segment");
+        Ptr<Instance> gote_jon = manager->instanceNew("gote_jon", "Truck segment");
+        jon_gote->attributeIs("return segment", "gote_jon");
+        jon_gote->attributeIs("source", "Jonkoping");
+        gote_jon->attributeIs("source", "Goteborg");
+        jon_gote->attributeIs("length", "150");
+        gote_jon->attributeIs("length", "150");
+        jon_gote->attributeIs("capacity", "6");
+        gote_jon->attributeIs("capacity", "6");
+
+        Ptr<Instance> gote_hys = manager->instanceNew("gote_hys", "Truck segment");
+        Ptr<Instance> hys_gote = manager->instanceNew("hys_gote", "Truck segment");
+        gote_hys->attributeIs("return segment", "hys_gote");
+        gote_hys->attributeIs("source", "Goteborg");
+        hys_gote->attributeIs("source", "Hyssna");
+        gote_hys->attributeIs("length", "50");
+        hys_gote->attributeIs("length", "50");
+        gote_hys->attributeIs("capacity", "2");
+        hys_gote->attributeIs("capacity", "2");
+
+        Ptr<Instance> gote_gotl = manager->instanceNew("gote_gotl", "Plane segment");
+        Ptr<Instance> gotl_gote = manager->instanceNew("gotl_gote", "Plane segment");
+        gote_gotl->attributeIs("return segment", "gotl_gote");
+        gote_gotl->attributeIs("source", "Goteborg");
+        gotl_gote->attributeIs("source", "Gotland");
+        gote_gotl->attributeIs("length", "400");
+        gotl_gote->attributeIs("length", "400");
+        gote_gotl->attributeIs("capacity", "1");
+        gotl_gote->attributeIs("capacity", "1");
+
+        conn->attributeIs("routing", "djikstras");
+
+        // jek
+
+        //timeManager->attributeIs("now", "50");
+
+        std::cout << "Done!" << std::endl;
+        
+    } catch (Fwk::Exception e) {
+        std::cerr << "Exception in main: " << e.what() << std::endl;
+    }
+    return 0;
+}
